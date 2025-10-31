b80d865e0f3a1e23ab656df8651e54c2b11e4b0d
lico <107700554+lico-world@users.noreply.github.com>
2025-10-31 08:25:42 +0100

    Sponge functions (#4)
    * Absorb and absorbLast functions + test draft (not passing)
    
    * Test correction and weak attribute to mock in tests
    
    * Commented mock function clean up
    
    * Tests for aborb function
    
    * Tests for the squeeze function
    
    * Test update & squeeze implementation

--- src/hash_utils.cpp
+++ src/hash_utils.cpp
@@ -1,5 +1,7 @@
 #include "../include/hash_utils.h"
 
+#include <iostream>
+
 namespace hash {
 namespace utils {
 namespace bit_management {
@@ -52,6 +54,7 @@ const uint64_t RC[24] = {
 
 } // namespace permutations_helpers
 
+__attribute__((weak)) // Allowing override in tests
 void keccakf(uint64_t state[25])
 {
     // 64bits -> 12 + 2*log2(64) = 24 rounds
@@ -128,13 +131,81 @@ void iota(uint64_t state[25], unsigned round)
 namespace sponge {
 
 void absorb(uint64_t state[25], const uint8_t* input, size_t inlen, size_t rate)
-{}
+{
+    size_t blockSize = rate / 8;
+    size_t offset = 0;
+
+    while(inlen >= blockSize)
+    {
+        for(size_t i=0 ; i<blockSize/8 ; i++)
+        {
+            state[i] ^= bit_management::load64(input + offset + 8*i);
+        }
+        permutations::keccakf(state);
+        offset += blockSize;
+        inlen -= blockSize;
+    }
+
+    if(inlen > 0)
+    {
+        absorbLastBlock(state, input + offset, inlen, rate);
+    }
+}
 
 void absorbLastBlock(uint64_t state[25], const uint8_t* last, size_t lastLen, size_t rate)
-{}
+{
+    size_t blockSize = rate / 8;
+    uint8_t temp[blockSize];
+    std::fill(temp, temp + blockSize, 0);
+    std::copy(last, last + lastLen, temp);
+
+    temp[lastLen] = 0x06;
+    temp[blockSize - 1] |= 0x80;
+
+    for(size_t i=0 ; i<blockSize/8 ; i++)
+    {
+        state[i] ^= bit_management::load64(temp + 8*i);
+    }
+    
+    permutations::keccakf(state);
+}
 
 void squeeze(uint64_t state[25], uint8_t* output, size_t outlen, size_t rate)
-{}
+{
+    if (outlen == 0) return;
+
+    size_t blockSize = rate /8;
+    size_t offset = 0;
+
+    while (outlen > 0)
+    {
+        size_t bytesToCopy = std::min(outlen, blockSize);
+
+        size_t fullLanes = bytesToCopy / 8;
+        for (size_t i = 0; i < fullLanes; i++)
+        {
+            bit_management::store64(output + offset + 8*i, state[i]);
+        }
+
+        size_t leftoverBytes = bytesToCopy % 8;
+        if (leftoverBytes > 0)
+        {
+            uint64_t lane = state[fullLanes];
+            for (size_t i = 0; i < leftoverBytes; i++)
+            {
+                output[offset + fullLanes*8 + i] = (lane >> (8*i)) & 0xFF;
+            }
+        }
+
+        offset += bytesToCopy;
+        outlen -= bytesToCopy;
+
+        if (outlen > 0)
+        {
+            permutations::keccakf(state);
+        }
+    }
+}
 
 
 } // namespace sponge
---
+++ test/src/sponge_test.cpp
@@ -0,0 +1,210 @@
+#include "../../include/hash_utils.h"
+
+#include <gtest/gtest.h>
+
+namespace hash {
+namespace utils {
+namespace permutations {
+
+void keccakf(uint64_t state[25])
+{
+    state[0] ^= 0xDEADBEEFDEADBEEF;
+}
+
+}}}
+
+class AbsorbLastBlockTest : public ::testing::Test
+{
+protected:
+    uint64_t state[25];
+    void SetUp() override
+    {
+        for(int i = 0; i < 25; i++) state[i] = 0;
+    }
+};
+
+TEST_F(AbsorbLastBlockTest, EmptyLastBlock) {
+    uint8_t input[1] = {};
+    hash::utils::sponge::absorbLastBlock(state, input, 0, 64);
+
+    uint64_t expected = 0x8000000000000006ULL;
+    EXPECT_EQ(state[0] & 0xFFFFFFFFFFFFFFFFULL, expected ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(AbsorbLastBlockTest, SmallLastBlock) {
+    uint8_t input[3] = {0xAA, 0xBB, 0xCC};
+    hash::utils::sponge::absorbLastBlock(state, input, 3, 64);
+
+    uint8_t temp[8] = {0xAA, 0xBB, 0xCC, 0x06, 0x00, 0x00, 0x00, 0x80};
+    uint64_t expected = hash::utils::bit_management::load64(temp);
+    EXPECT_EQ(state[0], expected ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(AbsorbLastBlockTest, NearFullBlock) {
+    uint8_t input[7] = {1, 2, 3, 4, 5, 6, 7};
+    hash::utils::sponge::absorbLastBlock(state, input, 7, 64);
+
+    uint8_t temp[8] = {1, 2, 3, 4, 5, 6, 7, 0x86};
+    uint64_t expected = hash::utils::bit_management::load64(temp);
+    EXPECT_EQ(state[0], expected ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(AbsorbLastBlockTest, TwoLanes) {
+    uint8_t input[10];
+    for (int i = 0; i < 10; i++) input[i] = i + 1;
+
+    hash::utils::sponge::absorbLastBlock(state, input, 10, 128);
+
+    uint8_t temp[16] = {};
+    std::copy(input, input + 10, temp);
+    temp[10] = 0x06;
+    temp[15] |= 0x80;
+
+    uint64_t expected0 = hash::utils::bit_management::load64(temp);
+    uint64_t expected1 = hash::utils::bit_management::load64(temp + 8);
+
+    EXPECT_EQ(state[0], expected0 ^ 0xDEADBEEFDEADBEEF);
+    EXPECT_EQ(state[1], expected1);
+}
+
+TEST_F(AbsorbLastBlockTest, PermutationApplied) {
+    uint8_t input[2] = {0x11, 0x22};
+    hash::utils::sponge::absorbLastBlock(state, input, 2, 64);
+    EXPECT_NE(state[0], 0ULL);
+}
+
+class AbsorbTest : public ::testing::Test
+{
+protected:
+    uint64_t state[25];
+
+    void SetUp() override
+    {
+        for (int i = 0; i < 25; i++) state[i] = 0;
+    }
+};
+
+TEST_F(AbsorbTest, SingleFullBlock)
+{
+    uint8_t input[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    hash::utils::sponge::absorb(state, input, sizeof(input), 64);
+
+    uint64_t expected = hash::utils::bit_management::load64(input);
+    EXPECT_EQ(state[0], expected ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(AbsorbTest, MultipleFullBlocks)
+{
+    uint8_t input[16];
+    for (int i = 0; i < 16; i++) input[i] = i + 1;
+
+    hash::utils::sponge::absorb(state, input, sizeof(input), 64);
+
+    uint64_t firstBlock = hash::utils::bit_management::load64(input);
+    uint64_t secondBlock = hash::utils::bit_management::load64(input + 8);
+
+    uint64_t expected = firstBlock ^ secondBlock;
+    EXPECT_EQ(state[0], expected);
+}
+
+TEST_F(AbsorbTest, PartialFinalBlock)
+{
+    uint8_t input[5] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
+    hash::utils::sponge::absorb(state, input, sizeof(input), 64);
+
+    uint8_t temp[8] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0x06, 0x00, 0x80};
+    uint64_t expected = hash::utils::bit_management::load64(temp);
+    EXPECT_EQ(state[0], expected ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(AbsorbTest, EmptyInput)
+{
+    uint8_t input[1] = {};
+    hash::utils::sponge::absorb(state, input, 0, 64);
+    for (int i = 0; i < 25; i++)
+        EXPECT_EQ(state[i], 0ULL);
+}
+
+TEST_F(AbsorbTest, TwoLaneRate)
+{
+    uint8_t input[16];
+    for (int i = 0; i < 16; i++) input[i] = i + 1;
+
+    hash::utils::sponge::absorb(state, input, sizeof(input), 128);
+
+    uint64_t lane0 = hash::utils::bit_management::load64(input);
+    uint64_t lane1 = hash::utils::bit_management::load64(input + 8);
+
+    EXPECT_EQ(state[0], lane0 ^ 0xDEADBEEFDEADBEEF);
+    EXPECT_EQ(state[1], lane1);
+}
+
+TEST_F(AbsorbTest, MixedFullAndPartialBlocks)
+{
+    uint8_t input[12];
+    for (int i = 0; i < 12; i++) input[i] = i + 1;
+
+    hash::utils::sponge::absorb(state, input, sizeof(input), 64);
+
+    uint8_t temp[8] = {9, 10, 11, 12, 0x06, 0x00, 0x00, 0x80};
+    uint64_t expected = hash::utils::bit_management::load64(temp);
+    expected ^= hash::utils::bit_management::load64(input);
+
+    EXPECT_EQ(state[0], expected);
+}
+
+class SqueezeTest : public ::testing::Test
+{
+protected:
+    uint64_t state[25];
+
+    void SetUp() override
+    {
+        for (int i = 0; i < 25; i++)
+            state[i] = i + 1;
+    }
+};
+
+TEST_F(SqueezeTest, SingleRateBlock)
+{
+    uint8_t output[8] = {};
+    hash::utils::sponge::squeeze(state, output, sizeof(output), 64);
+
+    uint64_t outValue = hash::utils::bit_management::load64(output);
+    EXPECT_EQ(outValue, state[0]);
+}
+
+TEST_F(SqueezeTest, TwoLaneRate)
+{
+    uint8_t output[16] = {};
+    hash::utils::sponge::squeeze(state, output, sizeof(output), 128);
+
+    uint64_t lane0 = hash::utils::bit_management::load64(output);
+    uint64_t lane1 = hash::utils::bit_management::load64(output + 8);
+
+    EXPECT_EQ(lane0, state[0]);
+    EXPECT_EQ(lane1, state[1]);
+}
+
+TEST_F(SqueezeTest, LongerThanRateCallsPermutation)
+{
+    uint8_t output[16] = {};
+    uint64_t originalState = state[0];
+
+    hash::utils::sponge::squeeze(state, output, sizeof(output), 8*8);
+
+    uint64_t firstOut = hash::utils::bit_management::load64(output);
+    uint64_t secondOut = hash::utils::bit_management::load64(output + 8);
+
+    EXPECT_EQ(firstOut, originalState);
+    EXPECT_EQ(secondOut, originalState ^ 0xDEADBEEFDEADBEEF);
+}
+
+TEST_F(SqueezeTest, ZeroOutputLength)
+{
+    uint8_t output[1] = {};
+    hash::utils::sponge::squeeze(state, output, 0, 64);
+    
+    for (int i = 0; i < 25; i++)
+        EXPECT_EQ(state[i], i + 1);
+}
